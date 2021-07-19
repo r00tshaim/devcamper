dbda5c3ce55bc91dcfe14550b0963802e85ce8a9
r00tshaim <shaim456@gmail.com>
2021-07-19 15:51:43 +0530

    Inital middleware setup

---
+++ middleware/logger.js
@@ -0,0 +1,7 @@
+// @desc        Logs requests to console
+const logger = (req, res, next) => {
+    console.log(`${req.method}   ${req.protocol}://${req.get('host')}${req.originalURL}`);
+    next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -8,6 +9,11 @@ dotenv.config({ path: './config/config.env' });
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
