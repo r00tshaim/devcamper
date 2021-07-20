f376cee9ef12817d750f4931cae3254bd60019fe
r00tshaim <shaim456@gmail.com>
2021-07-20 14:42:14 +0530

    Connecting Atlas Mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
@@ -7,6 +8,9 @@ const path = require('path');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -25,7 +29,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server
+    server.close(() => process.exit(1));
+});
