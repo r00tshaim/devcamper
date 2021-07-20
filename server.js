c8535f95afcf31db60a93787c0dfb900ba10f4c1
r00tshaim <shaim456@gmail.com>
2021-07-20 14:46:43 +0530

    Adding colors to console

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const colors = require('colors');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
@@ -31,12 +32,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     // Close server
     server.close(() => process.exit(1));
 });
