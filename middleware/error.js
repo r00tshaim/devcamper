a84832a66d09a5aba2edb937e4598a60b0e97ab3
r00tshaim <shaim456@gmail.com>
2021-07-25 18:53:13 +0530

    Mongoose Error Handling [2]

--- middleware/error.js
+++ middleware/error.js
@@ -7,12 +7,24 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     // Log to console for dev
-    console.log(err.stack.red);
+    console.log(err);
 
     // Mongoose bad ObjectId
     if (err.name === 'CastError') {
         const message = `Bootcamp not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose dupliate key (occurs during creating resource)
+    if (err.name === 'MongoError') {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error (occurs when some fields are missing while creating)
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
