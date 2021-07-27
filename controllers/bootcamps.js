851605ba4440a792f494767bfa730c70d9483f8a
r00tshaim <shaim456@gmail.com>
2021-07-27 16:51:15 +0530

    Get only selected fields of bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,12 +9,34 @@ exports.getBootcamps = async (req, res, next) => {
     try {
         //const bootcamps = await Bootcamp.find({});
         let query;
-        let queryStr = JSON.stringify(req.query);
+
+        // Copy req.query
+        const reqQuery = { ...req.query };
+
+        // Fields to exclude
+        const removeFields = ['select'];
+
+        // Loop over removeFields and delete them from reqQuery
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        // Create query string
+        let queryStr = JSON.stringify(reqQuery);
 
         //replacing gte with $gte - to form a mongoose query
+        // that is Create opertors like $gt, $gte, etc
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
+        // Finding resource
         query = Bootcamp.find(JSON.parse(queryStr));
+
+        // Select Fields
+        if (req.query.select) {
+            // Converting select fields to space seperted values ie: /api/v1/bootcamps?select=name,location to name location
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        // Executing query
         const bootcamps = await query;
         res.status(200).json({
             success: true,
