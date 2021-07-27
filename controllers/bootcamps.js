691730cd0f4aaa0065f90b1dbef057e74f0fc277
r00tshaim <shaim456@gmail.com>
2021-07-27 12:05:48 +0530

    Geospatial Query - Get Bootcamps Within Radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorResponse')
+const geocoder = require('../utils/geocoder')
 
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
@@ -89,6 +90,38 @@ exports.deleteBootcamp = async (req, res, next) => {
         }
 
         res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+        next(err);
+    }
+}
+
+
+// @desc        Get bootcamps within radius
+// @route       GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access      Private
+exports.getBootcampsInRadius = async (req, res, next) => {
+    try {
+        const { zipcode, distance } = req.params;
+
+        // Get lat/lng from geocoder
+        const loc = await geocoder.geocode(zipcode);
+        const lat = loc[0].latitude;
+        const lng = loc[0].longitude;
+
+        // Calc radius using radians
+        // Divide distance by radius of Earth
+        // Earth Radius = 3,963 miles / 6,378 kilometers
+        const radius = distance / 3963;
+
+        const bootcamps = await Bootcamp.find({
+            location: {
+                $geoWithin: {
+                    $centerSphere: [[lng, lat], radius]
+                }
+            }
+        });
+
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
     } catch (err) {
         next(err);
     }
