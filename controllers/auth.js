968c61591a01482db0eb4173d231bb6529417f9f
r00tshaim <shaim456@gmail.com>
2021-08-26 18:29:40 +0530

    User Register & Encrypting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,19 @@
-const Bootcamp = require('../models/User')
+const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
 
 // @desc        Register User
 // @route       POST /api/v1/auth/register
 // @access      Public
 exports.register = async (req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+
     res.status(200).json({ success: true });
 }
