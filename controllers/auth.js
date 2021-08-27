36dbd12a4911a84289e3611c473882d62fd66987
r00tshaim <shaim456@gmail.com>
2021-08-27 17:20:43 +0530

    Sign & Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,5 +15,8 @@ exports.register = async (req, res, next) => {
         role
     });
 
-    res.status(200).json({ success: true });
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token: token });
 }
