553fc35a4b31c71aca03e37f6b6b3356bbead59c
r00tshaim <shaim456@gmail.com>
2021-09-02 16:46:52 +0530

    Role Authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -33,3 +33,13 @@ exports.protect = async (req, res, next) => {
         next(err);
     }
 }
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 403));
+        }
+        next();
+    }
+}
